b656758828bccc0717060953b3ae1f4d79e1db5b
Wang Xiao <wx19930910@gmail.com>
2021-02-14 02:06:22 -0500

    refactor all cases

--- src/test/java/org/apache/commons/scxml2/semantics/SCXMLSemanticsImplTest.java
+++ src/test/java/org/apache/commons/scxml2/semantics/SCXMLSemanticsImplTest.java
@@ -16,10 +16,14 @@
  */
 package org.apache.commons.scxml2.semantics;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+
 import java.util.HashSet;
 import java.util.Set;
 
-import org.apache.commons.scxml2.env.MockErrorReporter;
+import org.apache.commons.scxml2.ErrorReporter;
 import org.apache.commons.scxml2.env.SimpleErrorReporter;
 import org.apache.commons.scxml2.model.EnterableState;
 import org.apache.commons.scxml2.model.Parallel;
@@ -29,82 +33,118 @@ import org.junit.jupiter.api.Test;
 
 public class SCXMLSemanticsImplTest {
 
-    @Test
-    public void testIsLegalConfigNoStates() {
-        final Set<EnterableState> states = new HashSet<>();
+	@Test
+	public void testIsLegalConfigNoStates() {
+		final Set<EnterableState> states = new HashSet<>();
 
-        Assertions.assertTrue(new SCXMLSemanticsImpl().isLegalConfiguration(states, new SimpleErrorReporter()));
-    }
+		Assertions.assertTrue(new SCXMLSemanticsImpl().isLegalConfiguration(states, new SimpleErrorReporter()));
+	}
 
-    @Test
-    public void testIsLegalConfigInvalidParallel() {
-        final Set<EnterableState> states = new HashSet<>();
-        final Parallel parallel = new Parallel();
+	@Test
+	public void testIsLegalConfigInvalidParallel() {
+		final Set<EnterableState> states = new HashSet<>();
+		final Parallel parallel = new Parallel();
 
-        final Parallel parent = new Parallel();
-        parent.setId("4");
+		final Parallel parent = new Parallel();
+		parent.setId("4");
 
-        final State state1 = new State();
-        state1.setId("1");
-        final State state2 = new State();
-        state2.setId("2");
+		final State state1 = new State();
+		state1.setId("1");
+		final State state2 = new State();
+		state2.setId("2");
 
-        parent.addChild(state1);
-        parent.addChild(state2);
+		parent.addChild(state1);
+		parent.addChild(state2);
 
-        parallel.setParent(parent);
+		parallel.setParent(parent);
 
-        states.add(parallel);
+		states.add(parallel);
 
-        final MockErrorReporter errorReporter = new MockErrorReporter();
+		final ErrorReporter errorReporter = mock(ErrorReporter.class);
+		Object[] errorReporterErrCtx = new Object[1];
+		String[] errorReporterErrCode = new String[1];
+		String[] errorReporterErrDetail = new String[1];
+		doAnswer((stubInvo) -> {
+			String errCode = stubInvo.getArgument(0);
+			String errDetail = stubInvo.getArgument(1);
+			Object errCtx = stubInvo.getArgument(2);
+			errorReporterErrCode[0] = errCode;
+			errorReporterErrDetail[0] = errDetail;
+			errorReporterErrCtx[0] = errCtx;
+			return null;
+		}).when(errorReporter).onError(any(), any(), any());
 
-        Assertions.assertFalse(new SCXMLSemanticsImpl().isLegalConfiguration(states, errorReporter));
-        Assertions.assertEquals(ErrorConstants.ILLEGAL_CONFIG, errorReporter.getErrCode());
-        Assertions.assertEquals("Not all AND states active for parallel 4", errorReporter.getErrDetail());
-    }
+		Assertions.assertFalse(new SCXMLSemanticsImpl().isLegalConfiguration(states, errorReporter));
+		Assertions.assertEquals(ErrorConstants.ILLEGAL_CONFIG, errorReporterErrCode[0]);
+		Assertions.assertEquals("Not all AND states active for parallel 4", errorReporterErrDetail[0]);
+	}
 
-    @Test
-    public void testIsLegalConfigMultipleTopLevel() {
-        final Set<EnterableState> states = new HashSet<>();
+	@Test
+	public void testIsLegalConfigMultipleTopLevel() {
+		final Set<EnterableState> states = new HashSet<>();
 
-        final State state1 = new State();
-        state1.setId("1");
-        final State state2 = new State();
-        state2.setId("2");
+		final State state1 = new State();
+		state1.setId("1");
+		final State state2 = new State();
+		state2.setId("2");
 
-        states.add(state1);
-        states.add(state2);
+		states.add(state1);
+		states.add(state2);
 
-        final MockErrorReporter errorReporter = new MockErrorReporter();
+		final ErrorReporter errorReporter = mock(ErrorReporter.class);
+		Object[] errorReporterErrCtx = new Object[1];
+		String[] errorReporterErrCode = new String[1];
+		String[] errorReporterErrDetail = new String[1];
+		doAnswer((stubInvo) -> {
+			String errCode = stubInvo.getArgument(0);
+			String errDetail = stubInvo.getArgument(1);
+			Object errCtx = stubInvo.getArgument(2);
+			errorReporterErrCode[0] = errCode;
+			errorReporterErrDetail[0] = errDetail;
+			errorReporterErrCtx[0] = errCtx;
+			return null;
+		}).when(errorReporter).onError(any(), any(), any());
 
-        Assertions.assertFalse(new SCXMLSemanticsImpl().isLegalConfiguration(states, errorReporter));
-        Assertions.assertEquals(ErrorConstants.ILLEGAL_CONFIG, errorReporter.getErrCode());
-        Assertions.assertEquals("Multiple top-level OR states active!", errorReporter.getErrDetail());
-    }
+		Assertions.assertFalse(new SCXMLSemanticsImpl().isLegalConfiguration(states, errorReporter));
+		Assertions.assertEquals(ErrorConstants.ILLEGAL_CONFIG, errorReporterErrCode[0]);
+		Assertions.assertEquals("Multiple top-level OR states active!", errorReporterErrDetail[0]);
+	}
 
-    @Test
-    public void testIsLegalConfigMultipleStatesActive() {
-        final Set<EnterableState> states = new HashSet<>();
+	@Test
+	public void testIsLegalConfigMultipleStatesActive() {
+		final Set<EnterableState> states = new HashSet<>();
 
-        final State state1 = new State();
-        state1.setId("1");
+		final State state1 = new State();
+		state1.setId("1");
 
-        final State state2 = new State();
-        state2.setId("2");
+		final State state2 = new State();
+		state2.setId("2");
 
-        final State parent = new State();
-        parent.setId("parentid");
+		final State parent = new State();
+		parent.setId("parentid");
 
-        state2.setParent(parent);
-        state1.setParent(parent);
+		state2.setParent(parent);
+		state1.setParent(parent);
 
-        states.add(state1);
-        states.add(state2);
+		states.add(state1);
+		states.add(state2);
 
-        final MockErrorReporter errorReporter = new MockErrorReporter();
+		final ErrorReporter errorReporter = mock(ErrorReporter.class);
+		Object[] errorReporterErrCtx = new Object[1];
+		String[] errorReporterErrCode = new String[1];
+		String[] errorReporterErrDetail = new String[1];
+		doAnswer((stubInvo) -> {
+			String errCode = stubInvo.getArgument(0);
+			String errDetail = stubInvo.getArgument(1);
+			Object errCtx = stubInvo.getArgument(2);
+			errorReporterErrCode[0] = errCode;
+			errorReporterErrDetail[0] = errDetail;
+			errorReporterErrCtx[0] = errCtx;
+			return null;
+		}).when(errorReporter).onError(any(), any(), any());
 
-        Assertions.assertFalse(new SCXMLSemanticsImpl().isLegalConfiguration(states, errorReporter));
-        Assertions.assertEquals(ErrorConstants.ILLEGAL_CONFIG, errorReporter.getErrCode());
-        Assertions.assertEquals("Multiple OR states active for state parentid", errorReporter.getErrDetail());
-    }
+		Assertions.assertFalse(new SCXMLSemanticsImpl().isLegalConfiguration(states, errorReporter));
+		Assertions.assertEquals(ErrorConstants.ILLEGAL_CONFIG, errorReporterErrCode[0]);
+		Assertions.assertEquals("Multiple OR states active for state parentid", errorReporterErrDetail[0]);
+	}
 }
